591f4774e69e1b1c2dfc48e64a1f4b85d4bd19a5
Mathias Dahlström <mathias.dahlstrom@gmail.com>
2025-04-02 12:23:19 +0200

    fix: added default created_at tags and tags ignore after inital resource creation

--- common.tf
+++ common.tf
@@ -1,7 +1,8 @@
 locals {
   default_tags = {
-    module    = "tf-azure-cloudflared"
-    ManagedBy = "terraform"
+    module     = "tf-azure-cloudflared"
+    ManagedBy  = "terraform"
+    created_at = formatdate("YYYY-MM-DD HH:mm:ss", timestamp())
   }
 
   common_tags = merge(local.default_tags, var.tags)
